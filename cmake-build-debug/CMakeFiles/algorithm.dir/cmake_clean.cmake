d8849eeaa20c016962b423c8b91a11151327f898
liurong <liurong@zhanyunsoft>
2024-09-16 21:48:23 +0800

    二叉树

---
+++ cmake-build-debug/CMakeFiles/algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm.dir/main.c.o"
+  "CMakeFiles/algorithm.dir/main.c.o.d"
+  "algorithm"
+  "algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 //
-// Created by 堕落天使 on 2024/9/11.
+// Created by 堕落天使 on 2024/9/13.
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,10 +7,67 @@
 #define TRUE 1
 #define FALSE 0
 
+typedef struct TreeNode {
+    char data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
 
+// 创建树，前序插入
+void creatTree(TreeNode **tree) {
+    char data;
+    scanf("%c \n", &data);
+
+    if(data == '#') {
+        tree = NULL;
+    }
+    else {
+        *tree = (TreeNode *) malloc(sizeof (TreeNode));
+        (*tree) -> data = data;
+        creatTree(&(*tree) -> left);
+        creatTree(&(*tree) -> right);
+    }
+}
+
+void preOrder(TreeNode **tree) {
+    if(*tree == NULL) {
+        return;
+    }
+    else {
+        printf("%c -> ", (*tree) -> data);
+        preOrder(&(*tree)->left);
+        preOrder(&(*tree)->right);
+    }
+}
+
+void midOrder(TreeNode **tree) {
+    if(*tree == NULL) {
+        return;
+    }
+    else {
+        midOrder(&(*tree)->left);
+        printf("%c -> ", (*tree) -> data);
+        midOrder(&(*tree)->right);
+    }
+}
+
+void afterOrder(TreeNode **tree) {
+    if(*tree == NULL) {
+        return;
+    }
+    else {
+        afterOrder(&(*tree)->left);
+        afterOrder(&(*tree)->right);
+        printf("%c -> ", (*tree) -> data);
+    }
+}
 
 int main() {
-
+    TreeNode *tree;
+    creatTree(tree);
+    preOrder(tree);
+    midOrder(tree);
+    afterOrder(tree);
 
     return 0;
 }
